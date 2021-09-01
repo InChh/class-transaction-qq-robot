1813695e25ebe1061a1ea1a1ed4089cba4f946af
In_Chh <2319397152@qq.com>
2021-09-01 09:36:40 +0800

    删除不必要的import

--- src/main/java/com/github/classtransactionqqrobot/ClassTransactionQqRobotApplication.java
+++ src/main/java/com/github/classtransactionqqrobot/ClassTransactionQqRobotApplication.java
@@ -1,10 +1,8 @@
 package com.github.classtransactionqqrobot;
 
-import com.github.classtransactionqqrobot.listener.BaseListener;
 import love.forte.simbot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * 主启动类
--- src/test/java/com/github/classtransactionqqrobot/listener/BaseListenerTest.java
+++ src/test/java/com/github/classtransactionqqrobot/listener/BaseListenerTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class BaseListenerTest {
     @Autowired
-    BaseListener baseListener;
+    GroupListener baseListener;
 
     @Test
     public void baseListenerTest(){
